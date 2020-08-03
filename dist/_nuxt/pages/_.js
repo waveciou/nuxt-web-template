f6c0e80a86213de8c9f4b9ad6b8eddf9dbc728a9
丘惟夫 <waveciou@gmail.com>
2020-08-03 23:16:34 +0800

    generate

--- dist/_nuxt/pages/_.js
+++ dist/_nuxt/pages/_.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{214:function(n,e,t){"use strict";t.r(e);var o={asyncData:function(n){n.route,n.query;return(0,n.redirect)("/")}},r=t(5),component=Object(r.a)(o,void 0,void 0,!1,null,null,null);e.default=component.exports}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{226:function(n,e,t){"use strict";t.r(e);var o={asyncData:function(n){n.route,n.query;return(0,n.redirect)("/")}},r=t(8),component=Object(r.a)(o,void 0,void 0,!1,null,null,null);e.default=component.exports}}]);
