4d6e08a8c2d5ca6b26060d51797e42c5974165c0
丘惟夫 <waveciou@gmail.com>
2020-08-11 23:40:42 +0800

    website development

---
+++ components/lightbox.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="lightbox__overlay">
+    <div class="lightbox">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "~/assets/scss/utils/_utils.scss";
+
+  .lightbox__overlay {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    z-index: 4000;
+    background-color: rgba($color-black, 0.7);
+    transition: opacity 0.3s;
+  }
+
+  .lightbox {
+    min-width: 360px;
+    max-width: 100%;
+    min-height: 480px;
+    max-height: 100%;
+    padding: 1.5rem;
+    background-color: $color-white;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+</style>
--- pages/news/index.vue
+++ pages/news/index.vue
@@ -6,11 +6,13 @@
         <h1 class="title">{{ site.name }}</h1>
       </div>
     </div>
+    <lightbox-component></lightbox-component>
   </div>
 </template>
 
 <script>
 import navbar from '~/components/model/navbar.vue';
+import lightbox from '~/components/lightbox.vue';
 
 export default {
   data() {
@@ -33,6 +35,7 @@ export default {
   },
   components: {
     'navbar-component': navbar,
+    'lightbox-component': lightbox
   }
 }
 </script>
